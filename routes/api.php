599448b814679b208d8fc051f6d0f7cbbba56db2
celine <celinekerubo28@gmail.com>
2023-08-11 14:01:32 +0300

    laravel commit

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout',[AuthController::class,'logout']);
+
+// Route::get('/profile',[AuthController::class,'index']);
+// Route::get('/profile(id)',[AuthController::class,'show']);
+// Route::patch('/profile(id)',[AuthController::class,'update']);
+// Route::delete('/profile(id)',[AuthController::class,'destroy']);
+
+// Route::apiResource('profile',ProfileController::class)->middleware(auth:sanctum);
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::apiResource('profile', ProfileController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
